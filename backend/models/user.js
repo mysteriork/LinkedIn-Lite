a10b41864660eed2ad3b2dbe1b6479a126418771
rk's git <130220726+mysteriork@users.noreply.github.com>
2025-08-21 23:44:42 +0530

    Update user.js

--- backend/models/user.js
+++ backend/models/user.js
@@ -6,7 +6,7 @@ const userSchema = new mongoose.Schema({
   email: { type: String, unique: true },
   username:String,
   password:{type: String,unique:true},
-  newPassword:{type:String,default:null},
+  newPassword:{type:String,default:null,unique:true},
   bio: String,
 });
 
